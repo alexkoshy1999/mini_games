0c73da263a8f5b97b74f5a98693c15596708a0b1
Your Name01~C:/Users/Alex Koshy/AppData/Local/Programs/Python/Python311/python.exe <alexkoshy36@gmail.com>
2022-12-09 23:57:09 +0530

    Updated with Scores and invalid choice.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,39 +2,52 @@ import random
 
 def get_choices():
     player_choice = input("Enter a Choice (rock, paper, scissors, exit):")
-    options = ["rock","paper","scissors"]
-    computer_choice = random.choice(options)
-    choices = {"player": player_choice, "computer":computer_choice,"game":player_choice}
-    return choices
+    if player_choice in ("rock","paper","scissors","exit"):
+        options = ["rock","paper","scissors"]
+        computer_choice = random.choice(options)
+        choices = {"player": player_choice, "computer":computer_choice,"game":player_choice}
+        return choices
+    else:
+        choices = {"player": 0,"game":0}
+        print("Enter a valid choice!!!!!")
+        return choices
 
 
 def check_win(player, computer):
     print(f"You chose {player}, computer chose {computer}")
     if player == computer:
-        return "It's a tie!"
+        return 0,"It's a tie!"
     elif player=="rock":
         if computer=="scissors":
-            return "Rock smashes Scissors! You win"
+            return 'p',"Rock smashes Scissors! You win"
         else:
-            return "Paper covers rock! You lose."
+            return 'c',"Paper covers rock! You lose."
     elif player=="paper":
         if computer=="rock":
-            return "Paper covers rock! You win!"
+            return 'p',"Paper covers rock! You win!"
         else:
-            return "Scissors cut paper! You lose."
+            return 'c',"Scissors cut paper! You lose."
     elif player=="scissors":
         if computer=="rock":
-            return "Rock smashes Scissors! You lose!"
+            return 'c',"Rock smashes Scissors! You lose!"
         else:
-            return "Scissors cut paper! You win."
-
+            return 'p',"Scissors cut paper! You win."
+p,c,g=0,0,0
 game = input("Enter \"play\" or \"exit\":")
 while game == "play":
     choices = get_choices()
     if choices["game"]=="exit":
         break
-    result = check_win(choices["player"],choices["computer"])
-    print(result)
-    
-
+    if  choices["game"]!=0:
+        point,result = check_win(choices["player"],choices["computer"])
+        print(result)
+        if result!="None":
+            g+=1
+        if point=='p':
+            p+=1
+        if point=='c':
+            c+=1
+        print(f"Total game played {g}")
+        print(f"Player Score: {p}")
+        print(f"Computer Score: {c}")
 
